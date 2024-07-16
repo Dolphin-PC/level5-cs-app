685b1a23b3a6ea4942f66f8c1b7f928b4a3a9a5b
dolphin-pc <pcx474@gmail.com>
2024-07-16 15:15:05 +0900

    [DEPLOY]

--- src/components/molecules/CommentForm.tsx
+++ src/components/molecules/CommentForm.tsx
@@ -1,6 +1,6 @@
 import * as S from "@/styles/index.style";
 import { IComment } from "@/types/comment";
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface Props {
