3e3fdfe307717b38ba070b0bd4f13bda2f20d8b2
dolphin-pc <pcx474@gmail.com>
2024-07-18 12:31:33 +0900

    [DEPLOY] json-server URL변경

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_JSON_SERVER_URL,
 });
 
 api.interceptors.response.use(
--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,7 @@ import removeConsole from "vite-plugin-remove-console";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), removeConsole()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+
   resolve: {
     alias: {
       "@": "/src",
